57203c986ba3cd8dae79f10a917ecbc0916a49a6
Vaishnavi Mugale <vmugale18@@gmail.com>
2026-03-14 00:47:30 +0530

    fix login api request

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -16,14 +16,12 @@ function Login() {
 
       const res = await axios.post(
         "https://pg-management-system-fvqd.onrender.com/api/auth/login",
-        null, 
-        {
-              params: {
+      {
 
           username: username,
           password: password
         }
-      }
+      
       );
 
       // store login data
